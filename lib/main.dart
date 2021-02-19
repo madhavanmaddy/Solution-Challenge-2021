fb9cfa351c9205ef192b48892da5a06ec5d80f3c
Vishwak <vishwak2505@gmail.com>
2021-02-19 13:51:02 +0530

    Maddy Title

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mental Maddy',
       theme: ThemeData(
         // This is the theme of your application.
         //
