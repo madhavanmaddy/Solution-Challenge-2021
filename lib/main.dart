5d40774b2f6624466661c57de5edecc35836c605
Udai2010 <udai.prasd.5@gmail.com>
2021-02-27 22:38:25 +0530

    Phone Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:smart_bus/screens/authentication/authenticate.dart';
-import 'package:smart_bus/screens/wrapper.dart';
+import 'package:smart_bus/services/authservice.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
@@ -11,12 +9,12 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Authenticate(),
+      home: AuthService().handleAuth(),
     );
   }
 }
---
+++ lib/services/authservice.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:smart_bus/screens/dashboard.dart';
+import 'package:smart_bus/screens/loginpage.dart';
+
+class AuthService {
+  //Handles Auth
+  handleAuth() {
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            return DashboardPage();
+          } else {
+            return LoginPage();
+          }
+        });
+  }
+
+  //Sign out
+  signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  //SignIn
+  signIn(AuthCredential authCreds) {
+    FirebaseAuth.instance.signInWithCredential(authCreds);
+  }
+
+  signInWithOTP(smsCode, verId) {
+    AuthCredential authCreds =
+        PhoneAuthProvider.credential(verificationId: verId, smsCode: smsCode);
+    signIn(authCreds);
+  }
+}
