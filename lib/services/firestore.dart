0a4a4b6b0715361a955693b94be309d89c3c43dd
Madhavan S <gitmaddy.16@gmail.com>
2021-03-16 16:35:18 +0530

    Final Edit

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -3,11 +3,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 CollectionReference users = FirebaseFirestore.instance.collection('Users');
 Future<void> userSetup(String name, String mobile) async {
-  var search = FirebaseFirestore.instance
+  var search = await FirebaseFirestore.instance
       .collection('Users')
       .where("Mobile", isEqualTo: mobile)
       .get();
-  if (search == null) users.add({'Name': name, 'Mobile': mobile, 'Balance': 0});
+  if (search.size == 0)
+    users.add({'Name': name, 'Mobile': mobile, 'Balance': 0});
   return;
 }
 
